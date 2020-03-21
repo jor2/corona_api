2dbef483da0aa72ea3172a84aacc60c14dea5ab8
jor2 <jordanwilliams997@hotmail.com>
2020-03-21 17:04:25 +0000

    update bug + create tests

--- corona.py
+++ corona.py
@@ -1,13 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+from flask import Flask
 
 
 class CoronaAPI(object):
     def __init__(self, country='Ireland'):
-        self.country = country
+        self.country = ' '.join([word.capitalize() for word in country.split()])
         self.url = 'https://www.worldometers.info/coronavirus/'
-        self.response = requests.get(self.url)
-        self.soup = BeautifulSoup(self.response.content, 'html.parser')
+        self.response_html = requests.get(self.url).content
+        self.soup = BeautifulSoup(self.response_html, 'html.parser')
 
     @property
     def all_country_statistics(self):
@@ -30,5 +31,19 @@ class CoronaAPI(object):
         return f'{numbers[1]}\n------------------\nTotal cases: {numbers[2]}\nTotal recovered: {numbers[6]}\nTotal deaths: {numbers[4]}\n'
 
 
-print(CoronaAPI().total_stats)
-print(CoronaAPI('Italy').country_stats)
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return CoronaAPI().total_stats
+
+
+@app.route('/<string:country>', methods=['GET', 'POST'])
+def corona_filtered(country):
+    return CoronaAPI(country).country_stats
+
+
+if __name__ == '__main__':
+    print(CoronaAPI('IrelaN').country_stats)
+    # app.run(debug=True, use_reloader=True)
